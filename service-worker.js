0d030c69d2ea527b82c2480ee0563240d77b4d7e
saber2pr <saber2pr@gmail.com>
2021-10-07 17:52:53 +0800

    chore: update

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@
  * @Author: saber2pr
  * @Date: 2019-11-21 22:13:28
  * @Last Modified by: saber2pr
- * @Last Modified time: 2021-10-07 14:11:31
+ * @Last Modified time: 2021-10-07 17:52:39
  */
 const staticAssets = [
-  /** CODE START **/"/build/index.html","/build/index~493df0b3a3cac26903f92205cf5c.css","/build/index~493df0b3a3cac26903f92205cf5c.min.js","/build/style.1a3cac26903f92205cf5c.css","/build/vendor~index~493df0b3a3cac26903f92205cf5c.min.js"/** CODE END **/,
+  /** CODE START **/"/build/index.html", "/build/index~493df0b3a3cac26903f92205cf5c.css", "/build/index~493df0b3a3cac26903f92205cf5c.min.js", "/build/style.1a3cac26903f92205cf5c.css", "/build/vendor~index~493df0b3a3cac26903f92205cf5c.min.js"/** CODE END **/,
   '/',
   // icon
   '/static/icon/saber2pr-144x144.png',
@@ -16,37 +16,32 @@ const staticAssets = [
   '/static/style/dark.css',
 ]
 
-const cdnStaticAssets = staticAssets.filter(src => src !== '/').map(src => `https://cdn.jsdelivr.net/gh/saber2pr/saber2pr.github.io@master${src}`)
-
 const StaticCacheKey = 'saber2pr-pwa-static'
 const DynamicCacheKey = 'saber2pr-pwa-dynamic'
 
 self.addEventListener('install', event =>
   event.waitUntil(
-    caches.open(StaticCacheKey).then(cache => cache.addAll(staticAssets.concat(cdnStaticAssets)))
+    caches.open(StaticCacheKey).then(cache => cache.addAll(staticAssets))
   )
 )
 
-const filterUrl = url =>
-  !(
-    url.startsWith('https://saber2pr.top/blog/')
-    || url.startsWith('https://saber2pr.top/static/')
-    || cdnStaticAssets.includes(url)
-  )
+const filterUrl = url => !(url.startsWith('https://saber2pr.top/blog/') || url.startsWith('https://saber2pr.top/static/'))
 
 self.addEventListener('fetch', event => {
   const url = event.request.url
   // only https
   if (!url.startsWith('https:')) return
-  // cache list
-  if (filterUrl(url)) return
+
   // version control
   if (url.startsWith('https://saber2pr.top/static/data/version.json')) return
 
+  // match statics
   if (staticAssets.find(path => path !== '/' && url.includes(path))) {
-    event.respondWith(caches.match(event.request))
-    return
+    return event.respondWith(caches.match(event.request))
   }
+
+  // cache list
+  if (filterUrl(url)) return
 
   event.respondWith(
     caches.match(event.request).then(resFromCache => {
