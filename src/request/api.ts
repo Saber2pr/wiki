4745b813cd70ced25fea3dcb67ca4452c46bbce3
muhai <muhai.li@tuya.com>
2023-08-31 21:23:50 +0800

    chore: update

--- src/request/api.ts
+++ src/request/api.ts
@@ -1,3 +1,5 @@
+import { origin } from '../config'
+
 export namespace API {
   export const createAvatars = (name: string, size = 70) =>
     `https://avatars.githubusercontent.com/${name}?size=${size}`
@@ -12,7 +14,26 @@ export namespace API {
     username: string,
     repo: string,
     path: string
-  ) => `https://github.com/${username}/${repo}/blob/master${path}`
-  export const createNewHref = (username: string, repo: string, path: string) =>
-    `https://github.com/${username}/${repo}/new/master/blog?filename=${`${path}/文章标题.md`}&value=${'文章正文'}`
+  ) => {
+    if (origin.isWiki) {
+      return `https://github.com/${username}/${repo}/edit/master${path.replace(
+        '/blog',
+        ''
+      )}`
+    }
+    return `https://github.com/${username}/${repo}/blob/master${path}`
+  }
+  export const createNewHref = (
+    username: string,
+    repo: string,
+    path: string
+  ) => {
+    if (origin.isWiki) {
+      return `https://github.com/${username}/${repo}/new/master?filename=${`${path.replace(
+        '/blog/',
+        ''
+      )}/文章标题.md`}&value=${'文章正文'}`
+    }
+    return `https://github.com/${username}/${repo}/new/master/blog?filename=${`${path}/文章标题.md`}&value=${'文章正文'}`
+  }
 }
