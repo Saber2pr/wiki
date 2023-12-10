5fa9bcf7edce5dd57fb1f604136a406645868087
muhai <muhai.li@tuya.com>
2023-12-10 14:14:42 +0800

    chore: update

--- webpack.wiki.config.js
+++ webpack.wiki.config.js
@@ -1,14 +1,10 @@
-const config = require('./app.json')
-
 const path = require('path')
 const { createConfig } = require('./webpack.config.base')
-
-const cdnhost = `//cdn.jsdelivr.net/gh/${config.userId}`
 
 const webpackConfig = createConfig({ isApp: false })
 
 webpackConfig.entry.index = './src/wiki.tsx'
-webpackConfig.output.publicPath = `${cdnhost}/${config.repo}@master/release/`
+webpackConfig.output.publicPath = `//saber2pr.top/wiki/release/`
 webpackConfig.output.path = path.join(__dirname, 'release')
 
 module.exports = webpackConfig
