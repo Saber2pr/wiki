4f577b30e2f55be50ece7aed12a8952d0a9ba19f
muhai <muhai.li@tuya.com>
2023-12-10 11:20:45 +0800

    chore: update

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -191,7 +191,9 @@ export const Blog = React.forwardRef<HTMLElement, Blog>(
               <>
                 <h1 className="Blog-Main-Title">{document.title}</h1>
                 <div className="Blog-Main-Content">
-                  <Md2jsx theme={md_theme}>{window.__blog}</Md2jsx>
+                  <Md2jsx basename={window.__basename || ''} theme={md_theme}>
+                    {window.__blog}
+                  </Md2jsx>
                 </div>
               </>
             ) : (
