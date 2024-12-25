e003aa7567a3652ee7679022d225f6d53bfe63ef
muhai <muhai.li@tuya.com>
2024-12-25 10:01:26 +0800

    chore: update

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -2,8 +2,6 @@ import './style.less'
 
 import React from 'react'
 
-import { freeCache } from '../../utils'
-
 export type ErrorInfo = { componentStack: string }
 export type Error = { message?: string; stack?: string }
 
@@ -18,48 +16,11 @@ export class ErrorBoundary extends React.Component<
 
   componentDidCatch(error: Error, info: ErrorInfo) {
     this.setState({ hasError: true, error, info })
-    freeCache('STATIC').then(() => freeCache('DYNAMIC'))
   }
 
   render() {
     if (this.state.hasError) {
-      return (
-        <dl className="ErrorBoundary">
-          <dt>
-            <h1>出现问题了QaQ！！不急，按照提示检查一下！</h1>
-          </dt>
-          <dd>{this.state.error.message}</dd>
-          <dd>
-            <pre>{this.state.error.stack}</pre>
-          </dd>
-          <dd>
-            <pre>{this.state.info.componentStack}</pre>
-          </dd>
-          <dd>
-            <p>
-              <a
-                className="AnchorHigh"
-                href="https://github.com/Saber2pr/saber2pr.github.io/issues/new"
-              >
-                0. 在github上反馈问题
-              </a>
-            </p>
-            <p>1. 检查设备是否已连接网络</p>
-            <p>
-              <a
-                className="AnchorHigh"
-                onClick={() => {
-                  freeCache('STATIC').then(() => freeCache('DYNAMIC'))
-                  location.reload()
-                }}
-              >
-                2. 清除浏览器缓存
-              </a>
-            </p>
-            <p>3. 刷新浏览器看看</p>
-          </dd>
-        </dl>
-      )
+      console.log('err', this.state)
     }
     return this.props.children
   }
