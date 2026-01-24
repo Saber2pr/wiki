9dcc06c213a6d4f7393c943fd7c309f509ab493b
saber2pr <muhai.li@tuya.com>
2026-01-24 18:18:03 +0800

    build(*): update

--- src/wiki.tsx
+++ src/wiki.tsx
@@ -11,6 +11,7 @@ import ReactDOM from 'react-dom'
 
 // /
 import { NavLink } from '@saber2pr/react-router'
+import { initAIAssistant } from '@saber2pr/ai-assistant'
 
 import { ErrorBoundary, SearchInput, Themer } from './components'
 import { getCurrentLang, I18nSelect } from './components/i18n-select'
@@ -46,6 +47,26 @@ const AppNavLink = ({
     }}
   />
 )
+
+
+// 默认执行初始化
+initAIAssistant({
+  locale: 'en-US'
+  // welcomeMessage: '有什么可以帮忙的？',
+  // suggestions: ['如何用 Typescript 实现 Helloworld？', '物联网是什么？'],
+  // placeholder: '给 GPT 发送消息',
+  // emptyMessage: '我是AI，可以回答你的问题，请在下方输入框输入你的需求～',
+  // async onBeforeChat(messages) {
+  //   const knowledgeContent = await fetch('http://localhost:5001/HTML超文本标记语言/移动端禁用双指放大.md').then(res => res.text())
+  //   return [
+  //     {
+  //       role: "system",
+  //       content: `你是我的博客助手，根据我博客内容回答：移动端禁用双指放大的方法：\n${knowledgeContent}`
+  //     },
+  //     ...messages
+  //   ]
+  // },
+});
 
 export const App = ({ blogTree }: App) => {
   const firstBlog = queryRootFirstChildMemo(blogTree)
@@ -160,6 +181,10 @@ const createWiki = (repo: string) => {
   origin.isWiki = true
   const current = getCurrentLang()
   i18n.setLocal(current.lang || 'en')
+
+  initAIAssistant({
+    locale: current?.lang === 'zh' ? 'zh-CN' : 'en-US'
+  })
 
   const host = location.host || ''
   const result = host.match(/^([\s\S]*?)\.github\.io$/)
